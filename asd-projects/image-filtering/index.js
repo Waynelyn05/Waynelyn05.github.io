22d6779df0cd066822632aea52d8efec9d7043db
Waynelyn05 <Spxz0utt@gmail.com>
2022-11-03 21:31:59 +0000

    saving data shapes

---
+++ asd-projects/image-filtering/index.js
@@ -0,0 +1,60 @@
+// This is a small program. There are only two sections. This first section is what runs
+// as soon as the page loads.
+$(document).ready(function () {
+  render($("#display"), image);
+  $("#apply").on("click", applyAndRender);
+  $("#reset").on("click", resetAndRender);
+});
+
+/////////////////////////////////////////////////////////
+//////// event handler functions are below here /////////
+/////////////////////////////////////////////////////////
+
+// this function resets the image to its original value; do not change this function
+function resetAndRender() {
+  reset();
+  render($("#display"), image);
+}
+
+// this function applies the filters to the image and is where you should call
+// all of your apply functions
+function applyAndRender() {
+  // Multiple TODOs: Call your apply function(s) here
+  applyFilter()
+
+
+  // do not change the below line of code
+  render($("#display"), image);
+}
+
+/////////////////////////////////////////////////////////
+// "apply" and "filter" functions should go below here //
+/////////////////////////////////////////////////////////
+
+// TODO 1, 2 & 4: Create the applyFilter function here
+function applyFilter() {
+  for (var i = 0; i < image.length; i++) {
+    var row = image[i];
+    for (var k = 0; k < row.length; k++) {
+      var rgbString = row[k]
+      var rgbNumbers = rgbStringToArray(rgbString)
+      rgbNumbers[RED] = 255;
+      rgbString = rgbArrayToString(rgbNumbers) //call the Array String with an argument inside.
+      row[k] = rgbString //assign the the string back to its array
+
+    }
+  }
+}
+// TODO 7: Create the applyFilterNoBackground function
+
+
+// TODO 5: Create the keepInBounds function
+
+
+// TODO 3: Create reddify function 
+function reddify()
+
+// TODO 6: Create more filter functions
+
+
+// CHALLENGE code goes below here
